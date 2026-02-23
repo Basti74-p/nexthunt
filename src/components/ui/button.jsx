6848b77f2494ca04a25aeee96c0baec24f585af0
Sebastian Pedde <sebastianpedde2@gmail.com>
2026-02-23 20:06:08 +0000

    File changes

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+          "bg-[#22c55e] text-black shadow hover:bg-[#16a34a]",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
