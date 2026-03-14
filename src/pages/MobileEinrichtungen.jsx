42694d9fa62677c7e3bd08830c45545040b9551e
Sebastian Pedde <sebastianpedde2@gmail.com>
2026-03-14 22:54:41 +0000

    File changes

--- src/pages/MobileEinrichtungen.jsx
+++ src/pages/MobileEinrichtungen.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "@/components/hooks/useAuth";
 import { base44 } from "@/api/base44Client";
